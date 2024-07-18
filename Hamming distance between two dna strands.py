b8daa4e00f02b47f5f9f38a90913a9f5946eb74b
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-07-18 12:38:17 -0400

    Update Hamming distance between two dna strands.py

--- Hamming distance between two dna strands.py
+++ Hamming distance between two dna strands.py
@@ -1,7 +1,11 @@
-seq11='GAGCCTACTAACGGGAT'
-seq21='CATCGTAATGACGGCCT'
-def ham_dist_fun(s1,s2):
-    return sum(c1!=c2 for c1,c2 in zip(s1,s2))
-print(ham_dist_fun(seq21,seq11))
+class find_distance:
+      def ham_dist_fun(s1,s2):
+          return sum(c1!=c2 for c1,c2 in zip(s1,s2))
+        
+if __name__ == "__main__":
+    seq11='GAGCCTACTAACGGGAT'
+    seq21='CATCGTAATGACGGCCT'
+
+    print(find_distance.ham_dist_fun(seq21,seq11))
            
         
